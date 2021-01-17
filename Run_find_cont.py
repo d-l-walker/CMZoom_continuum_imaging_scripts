84847968e89f542bca5efc9b7cf6b615578636f4
Daniel Walker <danielwalker274@gmail.com>
2021-01-17 11:45:57 +0000

    Tidied things up and added some more comments

--- Run_find_cont.py
+++ Run_find_cont.py
@@ -1,7 +1,7 @@
 """
-This script takes the command line input in order to generate a dirty cube for each sideband per correlator (asic and/or swarm) per track,
-using a coarse pixel size of 3 arcsec. Each dirty cube is then fed into the findContinuum script, which determines the line-free channels
-from an averaged spectrum.
+This script takes the command line input in order to generate a dirty cube for each sideband per correlator 
+(asic and/or swarm) per track, using a coarse pixel size of 3 arcsec. Each dirty cube is then fed into the 
+findContinuum script, which determines the line-free channels from an averaged spectrum.
 """
 
 import os
@@ -20,15 +20,21 @@ for filename in glob.iglob('*.fits'):
        t = os.path.splitext(filename)
        f = str(t[0])
        if not os.path.exists(f+'.ms'):
-           importuvfits(fitsfile=filename,vis=f+'.ms')
+           importuvfits(fitsfile=filename, vis=f+'.ms')
        else:
            continue
 
-imagesize=np.loadtxt('/reduction/czdata/dwalker/New/fcont_latest/Image_size_threshold.txt',dtype='str')
+imagesize = np.loadtxt('/reduction/czdata/dwalker/New/fcont_latest/Image_size_threshold.txt', dtype='str')
 
+"""
+Image size here is scaled since the cell size in tclean is smoothed to 3 arcsec, compared to 1 arcsec for 
+the final imaging. This is because the findContinuum routine takes a long time to run, especially with
+small pixels and hence a large image size. As we're only interested in the averaged spectrum, we don't 
+care to much about image resolution at this stage.
+"""
 for i in range(0,len(imagesize[:]),1):
         if sourcename in imagesize[i][0]:
-                ImageSize = [int(imagesize[i][2])/6,int(imagesize[i][2])/6] # Image size here is scaled since the cell size in tclean is smoothed to 3 arcsec, compared to 1 arcsec for the final imaging.
+                ImageSize = [int(imagesize[i][2])/6,int(imagesize[i][2])/6]
 
 for filename in glob.iglob('*.ms'):
         t = os.path.splitext(filename)
@@ -36,7 +42,9 @@ for filename in glob.iglob('*.ms'):
 
         if not os.path.exists(f+'.dirty.image'):
 
-            tclean(vis=filename,imagename=f+'.dirty',niter=0,gain=0.1,psfmode='hogbom',imagermode='mosaic',scaletype='SAULT',ftmachine='mosaic',interactive=False,imsize=ImageSize,cell="3arcsec",robust=0.5,weighting='briggs',stokes='I',chaniter=False,mode='frequency',phasecenter=Phase_Center)
+            tclean(vis=filename,imagename=f+'.dirty',niter=0,gain=0.1,psfmode='hogbom',imagermode='mosaic',
+                   scaletype='SAULT',ftmachine='mosaic',interactive=False,imsize=ImageSize,cell="3arcsec",
+                   robust=0.5,weighting='briggs',stokes='I',chaniter=False,mode='frequency',phasecenter=Phase_Center)
 
         if not os.path.exists(f+'.dirty.image_findContinuum.dat'):
 
